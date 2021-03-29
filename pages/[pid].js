e27ff7783aef20a36ed4706d293f99f33d494570
Epinder <epinder.singh@gmail.com>
2021-03-29 13:21:33 +0800

    error

---
+++ pages/[pid].js
@@ -0,0 +1,35 @@
+import { Fragment } from 'react';
+import path from 'path';
+import fs from 'fs/promises';
+
+function ProductDetailPage(props) {
+  const { loadedproduct } = props;
+  console.log(loadedproduct);
+
+  return (
+    <Fragment>
+      <h1>{loadedproduct.title}</h1>
+      <p>{loadedproduct.description}</p>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export default ProductDetailPage;
